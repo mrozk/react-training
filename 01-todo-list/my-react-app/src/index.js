6d313455f93898d63711826b8c118d897692bfd5
abandura <abandura@playtika.com>
2023-09-21 00:30:35 +0300

    initial commit

---
+++ 01-todo-list/my-react-app/src/index.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import TodoList from './TodoList';
+
+const todoList = [
+    { id: 1, text: 'Learn React', isCompleted: true },
+    { id: 2, text: 'Learn Redux', isCompleted: false },
+    { id: 3, text: 'Learn React Router', isCompleted: false },
+    { id: 4, text: 'Learn React Native', isCompleted: false },
+    { id: 5, text: 'Learn GraphQL', isCompleted: false },
+]
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+    <>
+        <h1>My todo list</h1>
+
+        <TodoList todos={todoList} />
+    </>
+);
